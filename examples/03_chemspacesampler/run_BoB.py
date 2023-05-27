2b832845ab334c5960e1559aeb7945caaa8fdc09
janweinreich <janweinreich286@gmail.com>
2023-05-27 13:04:21 +0200

    ...

--- examples/03_chemspacesampler/run_BoB.py
+++ examples/03_chemspacesampler/run_BoB.py
@@ -5,23 +5,23 @@ def main():
     params = {
         'min_d': 0.0,
         'max_d': 80.0,
-        'NPAR': 2,
-        'Nsteps': 20,
+        'NPAR':1,
+        'Nsteps': 30,
         'bias_strength': "none",
         'possible_elements': ["C", "O", "N", "F"],
         'not_protonated': None, 
         'forbidden_bonds': [(8, 9), (8, 8), (9, 9), (7, 7)],
-        'nhatoms_range': [13,16],
+        'nhatoms_range': [15,30],
         'betas': gen_exp_beta_array(4, 1.0, 32, max_real_beta=8.0),
         'make_restart_frequency': None,
         'rep_type': '3d',
         'rep_name': 'BoB',
-        'synth_cut': 2,
-        'ensemble': True,
+        'synth_cut': 7,
+        'ensemble': False,
         "verbose": True,
     }
 
-    MOLS, D= chemspace_potentials.chemspacesampler_BoB(smiles="CC(=O)OC1=CC=CC=C1C(=O)O", params=params)
+    MOLS, D= chemspace_potentials.chemspacesampler_BoB(smiles="CC(Cc1ccc(cc1)C(C(=O)O)C)C", params=params)
     print(MOLS)
     print(D)
 
