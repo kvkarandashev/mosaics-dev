f7fef8bee34ebb28dc630e6613b1db9feef45475
kvkarandashev <kvkarandashev@gmail.com>
2023-11-15 17:06:51 +0100

    Some more sanity checks.

---
+++ sanity_checks/crossover_move_intermediate_quantities/print_some_random_crossovers.py
@@ -0,0 +1,59 @@
+from mosaics.valence_treatment import str2ChemGraph
+import sys, random
+from mosaics.crossover import (
+    FragmentPair,
+    matching_status_reconnectors,
+    crossover_sample_random_outcome,
+)
+import numpy as np
+
+
+cg1 = str2ChemGraph(sys.argv[1])
+cg2 = str2ChemGraph(sys.argv[2])
+
+if len(sys.argv) > 3:
+    neighborhood_size1 = int(sys.argv[3])
+    neighborhood_size2 = int(sys.argv[4])
+else:
+    neighborhood_size1 = 3
+    neighborhood_size2 = 3
+
+origin_point1 = 0
+origin_point2 = 0
+
+
+print(cg1.hatoms)
+print(cg1.neighbors(origin_point1))
+
+print(cg2.hatoms)
+print(cg2.neighbors(origin_point2))
+
+frag_pair1 = FragmentPair(cg1, origin_point1, neighborhood_size=neighborhood_size1)
+
+frag_pair1.init_affected_status_info()
+
+print(frag_pair1.membership_vector)
+
+frag_pair2 = FragmentPair(cg2, origin_point2, neighborhood_size=neighborhood_size2)
+
+frag_pair2.init_affected_status_info()
+
+print(frag_pair2.membership_vector)
+
+reconnectors = matching_status_reconnectors(frag_pair1, frag_pair2)
+
+print("Reconnectors and statuses:")
+for recon, status_ids in zip(*reconnectors):
+    print(recon, status_ids)
+
+print("Several random crossover outcomes:")
+random.seed(1)
+np.random.seed(1)
+for _ in range(10):
+    print(
+        crossover_sample_random_outcome(
+            (cg1, cg2),
+            (neighborhood_size1, neighborhood_size2),
+            (origin_point1, origin_point2),
+        )
+    )
---
+++ sanity_checks/crossover_move_intermediate_quantities/print_some_random_crossovers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python print_some_random_crossovers.py "9@1:15@2@3:8:8" "6@1@2:7@2:7" 2 1
+
+python print_some_random_crossovers.py "9@1:15@2@3:8#1:8#1" "6#2@1@2:7@2:7" 2 1
