9323c4427e2a88279ac319cfdca54c3b6517ce16
kvkarandashev <kvkarandashev@gmail.com>
2026-02-02 14:24:17 +0100

    more charged phosphorus and sulfur support

--- mosaics/periodic.py
+++ mosaics/periodic.py
@@ -125,14 +125,20 @@ p_int = ElementDict(
 
 # Priority list of charged species.
 # Introduced in order charges to C were assigned only if options of fitting valences with adding charges to O and N were exhausted.
-charge_feasibility_list = [[5, 7, 8, 16], [], [], [6]]
-available_charges_lists = [{5: -1, 7: 1, 8: -1, 16: 1}, {8: 1, 16: -1}, {7: -1}, {6: -1}]
+charge_feasibility_list = [[5, 7, 8, 16], [], [], [6, 15]]
+available_charges_lists = [
+    {5: -1, 7: 1, 8: -1, 16: 1},
+    {8: 1, 16: -1},
+    {7: -1},
+    {6: -1, 15: (-1, 1)},
+]
 # which valences can be
 charged_valences_int = {
     5: {-1: 4},
     6: {-1: 3},
     7: {1: 4, -1: 2},
     8: {-1: 1, 1: 3},
+    15: {1: 4, -1: 2},
     16: {1: 3, -1: 1},
 }
 
